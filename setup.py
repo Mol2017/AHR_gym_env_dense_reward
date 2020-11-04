ce8edf53408ca828a8b70b2416b697e911e4dddf
Wentao <wentaowang23@gmail.com>
2020-11-04 18:42:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='gym_air_hockey',
       version='1.0.0',
       description='OpenAI Gym Environment Wrapper for Air Hockey Game Simulator',
-      author=['Akhmed Rakhmati', 'me'],
+      author=['Akhmed Rakhmati', 'AHR Group'],
       install_requires=['gym',
                         'pygame',
                         'numpy',
